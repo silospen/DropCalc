e9b3c5dbbe9daea25cea547833f53c15ab221cfb
silo <silospen@silospen.com>
2021-08-22 08:03:12 -0700

    Read tsv files

---
+++ src/test/kotlin/com/silospen/dropcalc/reader/FileReaderTest.kt
@@ -0,0 +1,17 @@
+package com.silospen.dropcalc.reader
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class FileReaderTest {
+    @Test
+    fun testReadTsv() {
+        val tempFile = File.createTempFile("fileReaderTest", null)
+        tempFile.deleteOnExit()
+        tempFile.writeText("header1\theader2\nval1\tval2\nval3\tval4")
+        val actual = readTsv(tempFile) { Pair(it[1], it[0]) }
+        val expected = listOf(Pair("val2", "val1"), Pair("val4", "val3"))
+        assertEquals(expected, actual)
+    }
+}
