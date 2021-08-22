bc6e146194a4bd3ae114bc03f9dbbe15ab7014ea
silo <silospen@silospen.com>
2021-08-22 14:19:38 -0700

    Skip null parsed records in tsv files

--- src/main/kotlin/com/silospen/dropcalc/reader/FileReader.kt
+++ src/main/kotlin/com/silospen/dropcalc/reader/FileReader.kt
@@ -7,5 +7,6 @@ fun <T> readTsv(file: File, lineParser: (List<String>) -> T): List<T> =
         lines.drop(1)
             .map { it.split('\t') }
             .map(lineParser)
+            .filter { it != null }
             .toList()
     }
--- src/test/kotlin/com/silospen/dropcalc/reader/FileReaderTest.kt
+++ src/test/kotlin/com/silospen/dropcalc/reader/FileReaderTest.kt
@@ -9,8 +9,8 @@ class FileReaderTest {
     fun testReadTsv() {
         val tempFile = File.createTempFile("fileReaderTest", null)
         tempFile.deleteOnExit()
-        tempFile.writeText("header1\theader2\nval1\tval2\nval3\tval4")
-        val actual = readTsv(tempFile) { Pair(it[1], it[0]) }
+        tempFile.writeText("header1\theader2\nval1\tval2\nval3\tval4\nignore\tignore")
+        val actual = readTsv(tempFile) { if (it.contains("ignore")) null else Pair(it[1], it[0]) }
         val expected = listOf(Pair("val2", "val1"), Pair("val4", "val3"))
         assertEquals(expected, actual)
     }
